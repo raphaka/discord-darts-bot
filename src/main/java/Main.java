36c19b5f08a90dd9e71d988a00add71a2c3d2125
Raphael Kaemper <raphi.kaemper@web.de>
2020-03-24 00:17:08 +0100

    JDA-Bot goes online

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws javax.security.auth.login.LoginException{
         System.out.println("hello darts bot");
+
+        JDA jda = new JDABuilder(args[0]).build();
+
     }
 }
 
