c3630b71809afc9ef1785acf3457f9120db6e46f
Raphael Kaemper <raphi.kaemper@web.de>
2020-05-21 18:21:29 +0200

    validate if start score can be set

--- src/main/java/events/StartScoreEvent.java
+++ src/main/java/events/StartScoreEvent.java
@@ -1,7 +1,10 @@
 package events;
 
+import Entities.Player;
 import Managers.MatchManager;
 import games.Match;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -19,22 +22,46 @@ public class StartScoreEvent extends ListenerAdapter {
         try{
             stsc = Integer.parseInt(msg[1]);
         } catch (java.lang.NumberFormatException e) {
+            event.getChannel().sendMessage("Start score cannot be set to ").append(msg[1]).queue();
             return;
             // if message cannot be parsed as an Integer, it is not meant to be processed by this handler
         }
         if (event.getGuild().getCategoriesByName("Dartboards", true).contains(event.getChannel().getParent())) {
             Match m = MatchManager.getInstance().getMatchByChannel(event.getChannel());
             if (m != null) {
-                if (m.isWaitingForStart()){
-                    if (stsc > 1) {
-                        if (stsc == 42 || stsc == 69 || stsc == 420 || stsc == 1337 || stsc == 80085){
-                            event.getChannel().sendMessage("NICE :)").queue();
-                        }
-                        m.setStartScore(stsc);
-                        event.getChannel().sendMessage("Start score has been set to ").append(String.valueOf(stsc)).queue();
-                    } else {
-                        event.getChannel().sendMessage("Start score cannot be set to ").append(String.valueOf(stsc)).queue();
+                //check if user is allowed to set start score
+                boolean allowed = false;
+                //check if user is a player of the game
+                for(Player p : m.getPlayers()){
+                    if (event.getAuthor().getId().equals(p.getId())) {
+                        allowed = true;
                     }
+                }
+                //check if player has role referee
+                if (event.getGuild().getRolesByName("Referee", true).size() != 0) {
+                    Role referee = event.getGuild().getRolesByName("Referee", true).get(0);
+                    for(Member mem : event.getGuild().getMembersWithRoles(referee)){
+                        if (event.getAuthor() == mem.getUser()) {
+                            allowed = true;
+                        }
+                    }
+                }
+                if(allowed) {
+                    if (m.isWaitingForStart()) {
+                        if (stsc > 1) {
+                            if (stsc == 42 || stsc == 69 || stsc == 420 || stsc == 1337 || stsc == 80085) {
+                                event.getChannel().sendMessage("NICE :)").queue();
+                            }
+                            m.setStartScore(stsc);
+                            event.getChannel().sendMessage("Start score has been set to ").append(String.valueOf(stsc)).queue();
+                        } else {
+                            event.getChannel().sendMessage("Start score cannot be set to ").append(String.valueOf(stsc)).queue();
+                        }
+                    } else {
+                        event.getChannel().sendMessage("The start score can only be set before the match ").queue();
+                    }
+                } else {
+                    event.getChannel().sendMessage("Only players of this match and users with the role \"Referee\" are allowed to set the start score").queue();
                 }
             }
         }
